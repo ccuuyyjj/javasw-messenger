0bce5b5da1f0937de8bebba55d8971daaffeff42
IT202-02 <IT202-02@DESKTOP-1S6SP9E>
2017-06-13 17:10:40 +0900

    Test

--- Messenger/src/test/Test01.java
+++ Messenger/src/test/Test01.java
@@ -1,9 +1,9 @@
 package test;
 
 import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import container.Friends;
 
@@ -13,11 +13,14 @@ public class Test01 {
 		f.getFriends().add("def");
 		f.getFriends().add("ghi");
 		System.out.println(f.getIdentity() + " " + f.getFriends());
-		File target = new File("testfile");
-		FileOutputStream out = new FileOutputStream(target);
-		BufferedOutputStream buffer = new BufferedOutputStream(out);
+//		File target = new File("testfile");
+//		FileOutputStream out = new FileOutputStream(target);
+		ServerSocket server = new ServerSocket(20000);
+		Socket socket = server.accept();
+		BufferedOutputStream buffer = new BufferedOutputStream(socket.getOutputStream());
 		ObjectOutputStream writer = new ObjectOutputStream(buffer);
 		writer.writeObject(f);
 		writer.close();
+		server.close();
 	}
 }
--- Messenger/src/test/Test02.java
+++ Messenger/src/test/Test02.java
@@ -1,17 +1,19 @@
 package test;
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 import container.Friends;
 
 public class Test02 {
 	public static void main(String[] args) throws Exception {
-		File target = new File("testfile");
-		FileInputStream in = new FileInputStream(target);
-		BufferedInputStream buffer = new BufferedInputStream(in);
+//		File target = new File("testfile");
+//		FileInputStream in = new FileInputStream(target);
+		InetAddress inet = InetAddress.getByName("warrock.iptime.org");
+		Socket socket = new Socket(inet, 20000);
+		BufferedInputStream buffer = new BufferedInputStream(socket.getInputStream());
 		ObjectInputStream writer = new ObjectInputStream(buffer);
 		Friends f = (Friends) writer.readObject();
 		writer.close();
